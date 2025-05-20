1e3a06baa3a263a2c32701d82beca88ee8588d54
Austin Horn <ahornster@gmail.com>
2025-05-20 14:24:23 -0400

    Project Setup, Scene Building, and More
    -Created the Project using Unity's 3D Core (Built-in Pipeline)
    
    -Created base templates for each Scene I expect to have
    -Created Scene Navigation Button Prefabs
    -Created GameManager Prefab
    -Created SceneController Script
    -Imported LeafBladeGames Logo (both transparent background and nontransparent versions)

---
+++ Dungeon Defense/Assets/_Scripts/SceneController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        StartCoroutine(LaunchRoutine());
+
+    }
+
+
+    IEnumerator LaunchRoutine()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("StudioScene");
+
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("MenuScene");
+
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("CreditScene");
+    }
+
+    public void LoadOptions()
+    {
+        SceneManager.LoadScene("OptionsScene");
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("GameplayScene");
+    }
+
+    public void LoadHowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlayScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadWin()
+    {
+        SceneManager.LoadScene("WinScene");
+    }
+    public void LoadLose()
+    {
+        SceneManager.LoadScene("LoseScene");
+    }
+}
